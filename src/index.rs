8be0059b9f120363e73afe0239f1701fa7a85ab9
jtb <jan@boee.org>
2023-01-29 14:23:57 +0100

    Compact rocksDb

--- src/index.rs
+++ src/index.rs
@@ -11,7 +11,7 @@ use druid::ExtEventSink;
 use melt_rs::get_search_index;
 use melt_rs::index::SearchIndex;
 use rayon::prelude::*;
-use rocksdb::{DB, DBWithThreadMode, Options, SingleThreaded};
+use rocksdb::{BlockBasedOptions, Cache, DB, DBCompactionStyle, DBCompressionType, DBWithThreadMode, Options, SingleThreaded};
 use crate::data::AppState;
 
 pub static GLOBAL_COUNT: AtomicUsize = AtomicUsize::new(0);
@@ -29,13 +29,48 @@ fn index_tread(rx_search: Receiver<CommandMessage>, tx_res: Sender<ResultMessage
     thread::spawn(move || {
         let buf = dirs::home_dir().unwrap().into_os_string().into_string().unwrap();
         let path = format!("{}/.melt.db", buf);
-        let mut opts = Options::default();
-        opts.create_if_missing(true);
-        opts.create_missing_column_families(true);
-        opts.increase_parallelism(8);
-        opts.optimize_for_point_lookup(10);
-        let conn: DBWithThreadMode<SingleThreaded> = DBWithThreadMode::open_cf(&opts, path, &["default"]).unwrap();
 
+        let cpu = num_cpus::get() as _;
+        let mut opt = Options::default();
+
+        opt.create_if_missing(true);
+        opt.set_use_fsync(false);
+        opt.set_compaction_style(DBCompactionStyle::Universal);
+        opt.set_disable_auto_compactions(false);
+        opt.increase_parallelism(cpu);
+        opt.set_max_background_jobs(cpu / 3 + 1);
+        opt.set_keep_log_file_num(16);
+        opt.set_level_compaction_dynamic_level_bytes(true);
+
+        opt.set_compression_type(DBCompressionType::Lz4);
+        opt.set_bottommost_compression_type(DBCompressionType::Zstd);
+        let dict_size = 32768;
+        let max_train_bytes = dict_size * 128;
+        opt.set_bottommost_compression_options(-14, 32767, 0, dict_size, true);
+        opt.set_bottommost_zstd_max_train_bytes(max_train_bytes, true);
+
+        opt.set_enable_blob_files(true);
+        opt.set_min_blob_size(4096);
+        opt.set_blob_file_size(268435456);
+        opt.set_blob_compression_type(DBCompressionType::Zstd);
+        opt.set_enable_blob_gc(true);
+        opt.set_blob_gc_age_cutoff(0.25);
+        opt.set_blob_gc_force_threshold(0.8);
+
+        opt.set_bytes_per_sync(8388608);
+        opt.optimize_for_point_lookup(1024 * 1024);
+
+        let cache = Cache::new_lru_cache(16 * 1024 * 1024).unwrap();
+        let mut bopt = BlockBasedOptions::default();
+        bopt.set_ribbon_filter(10.0);
+        bopt.set_block_cache(&cache);
+        bopt.set_block_size(6 * 1024);
+        bopt.set_cache_index_and_filter_blocks(true);
+        bopt.set_pin_l0_filter_and_index_blocks_in_cache(true);
+        opt.set_block_based_table_factory(&bopt);
+        opt.create_missing_column_families(true);
+
+        let conn: DBWithThreadMode<SingleThreaded> = DBWithThreadMode::open_cf(&opt, path, &["default"]).unwrap();
         let mut index = load_from_json();
         GLOBAL_COUNT.store(index.get_size(), Ordering::SeqCst);
         GLOBAL_SIZE.store(index.get_size_bytes() / 1_000_000, Ordering::SeqCst);
@@ -66,7 +101,9 @@ fn index_tread(rx_search: Receiver<CommandMessage>, tx_res: Sender<ResultMessage
                                     })
                                     .collect::<Vec<String>>());
                             });
-
+                            if processed > index_hits{
+                                processed = index_hits;
+                            }
                             let duration_db = start.elapsed();
                             let res_size = result.len();
                             tx_res.send(ResultMessage::Messages(result, format!("Index        {:?}\nIndex hits   {:?}\nRetrieve     {:?}\nProcessed    {:?}\nResults      {}", duration_index, index_hits, duration_db, processed, res_size))).unwrap();
