c787d0f43f679d461bf9618bfa2904ea58980c3a
jtb <jan@boee.org>
2023-02-04 08:50:05 +0100

    format

--- src/view.rs
+++ src/view.rs
@@ -1,12 +1,18 @@
 use std::sync::atomic::Ordering;
 
-use druid::{Env, Event, EventCtx, FontDescriptor, FontFamily, FontWeight, widget::{Button, Flex, Label, List}, Widget, widget::TextBox, WidgetExt};
 use druid::widget::{Checkbox, Container, Controller, Either, LineBreaking, Scroll, Slider, Split};
+use druid::{
+    widget::TextBox,
+    widget::{Button, Flex, Label, List},
+    Env, Event, EventCtx, FontDescriptor, FontFamily, FontWeight, Widget, WidgetExt,
+};
 
 use crate::data::*;
-use crate::delegate::{CHANGE_SETTINGS, CHECK_CLICKED_FOR_POINTER, CLEAR_DB, SEARCH, SET_VIEW_COLUMN};
-use crate::GLOBAL_STATE;
+use crate::delegate::{
+    CHANGE_SETTINGS, CHECK_CLICKED_FOR_POINTER, CLEAR_DB, SEARCH, SET_VIEW_COLUMN,
+};
 use crate::index::{CommandMessage, GLOBAL_COUNT};
+use crate::GLOBAL_STATE;
 
 fn new_search_textbox() -> impl Widget<AppState> {
     let new_search_textbox = TextBox::new()
@@ -23,10 +29,14 @@ fn new_search_textbox() -> impl Widget<AppState> {
     Flex::row()
         .with_flex_child(new_search_textbox.padding(5.), 1.)
         .with_flex_child(new_search_textbox_neq.padding(5.), 1.)
-        .with_child(Checkbox::new("Exact").lens(AppState::exact)).on_click(|ctx, data: &mut AppState, _env| {
-        ctx.submit_command(SEARCH.with(((data.query.to_string(), data.not_query.to_string()), data.exact)));
-        ctx.request_update()
-    })
+        .with_child(Checkbox::new("Exact").lens(AppState::exact))
+        .on_click(|ctx, data: &mut AppState, _env| {
+            ctx.submit_command(SEARCH.with((
+                (data.query.to_string(), data.not_query.to_string()),
+                data.exact,
+            )));
+            ctx.request_update()
+        })
 }
 
 struct SearchController;
@@ -34,7 +44,14 @@ struct SearchController;
 struct ControllerForNegSearch;
 
 impl<W: Widget<AppState>> Controller<AppState, W> for SearchController {
-    fn event(&mut self, child: &mut W, ctx: &mut EventCtx, event: &Event, data: &mut AppState, env: &Env) {
+    fn event(
+        &mut self,
+        child: &mut W,
+        ctx: &mut EventCtx,
+        event: &Event,
+        data: &mut AppState,
+        env: &Env,
+    ) {
         if let Event::WindowConnected = event {
             ctx.request_focus();
         }
@@ -44,7 +61,10 @@ impl<W: Widget<AppState>> Controller<AppState, W> for SearchController {
             data.prob = convert_to_ratio(prob as f64, 1., GLOBAL_COUNT.load(Ordering::SeqCst));
             GLOBAL_STATE.lock().unwrap().query = data.query.to_string();
             GLOBAL_STATE.lock().unwrap().query_neg = data.not_query.to_string();
-            ctx.submit_command(SEARCH.with(((data.query.to_string(), data.not_query.to_string()), data.exact)));
+            ctx.submit_command(SEARCH.with((
+                (data.query.to_string(), data.not_query.to_string()),
+                data.exact,
+            )));
         }
 
         child.event(ctx, event, data, env)
@@ -52,11 +72,21 @@ impl<W: Widget<AppState>> Controller<AppState, W> for SearchController {
 }
 
 impl<W: Widget<AppState>> Controller<AppState, W> for ControllerForNegSearch {
-    fn event(&mut self, child: &mut W, ctx: &mut EventCtx, event: &Event, data: &mut AppState, env: &Env) {
+    fn event(
+        &mut self,
+        child: &mut W,
+        ctx: &mut EventCtx,
+        event: &Event,
+        data: &mut AppState,
+        env: &Env,
+    ) {
         if let Event::KeyUp(_) = event {
             let prob = (data.index_prob as f32).powi(trigram(data.query.as_str()).len() as i32);
             data.prob = convert_to_ratio(prob as f64, 1., GLOBAL_COUNT.load(Ordering::SeqCst));
-            ctx.submit_command(SEARCH.with(((data.query.to_string(), data.not_query.to_string()), data.exact)));
+            ctx.submit_command(SEARCH.with((
+                (data.query.to_string(), data.not_query.to_string()),
+                data.exact,
+            )));
         }
 
         child.event(ctx, event, data, env)
@@ -67,104 +97,197 @@ fn convert_to_ratio(probability: f64, total: f64, index_size: usize) -> String {
     if probability == 0.0 {
         return "Total cannot be zero".to_string();
     }
-    format!("P index hits {}", index_size / (total / probability) as usize)
+    format!(
+        "P index hits {}",
+        index_size / (total / probability) as usize
+    )
 }
 
 fn documents() -> impl Widget<Item> {
-    let font = FontDescriptor::new(FontFamily::MONOSPACE)
-        .with_weight(FontWeight::BOLD);
-    let label = Label::raw().with_font(font).with_line_break_mode(LineBreaking::Clip)
-        .expand_width().lens(Item::view).on_click(Item::click_view);
+    let font = FontDescriptor::new(FontFamily::MONOSPACE).with_weight(FontWeight::BOLD);
+    let label = Label::raw()
+        .with_font(font)
+        .with_line_break_mode(LineBreaking::Clip)
+        .expand_width()
+        .lens(Item::view)
+        .on_click(Item::click_view);
 
-    Flex::row()
-        .with_flex_child(label, 1.)
+    Flex::row().with_flex_child(label, 1.)
 }
 
 pub fn build_ui() -> impl Widget<AppState> {
     let items = List::new(documents).lens(AppState::items);
     let flex = Flex::column()
-        .with_child(Flex::row().with_child(Button::new("Settings").on_click(|ctx, data: &mut AppState, _env| {
-            ctx.submit_command(CHANGE_SETTINGS.with(!data.settings));
-            ctx.request_update()
-        }).align_left()).with_child(Button::new("Clear").on_click(|ctx, _: &mut AppState, _env| {
-            ctx.submit_command(CLEAR_DB);
-            ctx.request_update()
-        }).align_right()))
-        .with_child(Label::raw().with_font(FontDescriptor::new(FontFamily::MONOSPACE)).lens(AppState::query_time).align_left())
-        .with_child(Label::raw().with_font(FontDescriptor::new(FontFamily::MONOSPACE)).lens(AppState::count).align_left())
-        .with_child(Label::raw().with_font(FontDescriptor::new(FontFamily::MONOSPACE)).lens(AppState::size).align_left())
-        .with_child(Label::raw().with_font(FontDescriptor::new(FontFamily::MONOSPACE)).lens(AppState::indexed_data_in_bytes_string).align_left())
-        .with_child(Label::raw().with_font(FontDescriptor::new(FontFamily::MONOSPACE)).lens(AppState::prob).align_left())
-        .with_child(Label::dynamic(|value: &AppState, _| {
-            format!("Index prob   {:.2}", value.index_prob)
-        }).with_font(FontDescriptor::new(FontFamily::MONOSPACE)).align_left())
-        .with_child(Slider::new()
-            .with_range(0.01, 0.8)
-            .with_step(0.01)
-            .lens(AppState::index_prob)
-            .align_left())
-        .with_child(Button::new("Set index prob").on_click(|_, data: &mut AppState, _env| {
-            data.tx.send(CommandMessage::SetProb(data.index_prob.clone())).unwrap();
-        }).align_left())
-        .with_child(Label::dynamic(|value: &AppState, _| {
-            format!("Time limit   {:?} ms", value.timelimit as u64)
-        }).with_font(FontDescriptor::new(FontFamily::MONOSPACE)).align_left())
-        .with_child(Slider::new()
-            .with_range(10.0, 10000.0)
-            .with_step(1.0)
-            .lens(AppState::timelimit)
-            .align_left())
-        .with_child(Label::dynamic(|value: &AppState, _| {
-            format!("View limit   {:?}", value.viewlimit as u64)
-        }).with_font(FontDescriptor::new(FontFamily::MONOSPACE)).align_left())
-        .with_child(Slider::new()
-            .with_range(100.0, 50000.0)
-            .with_step(1.0)
-            .lens(AppState::viewlimit)
-            .align_left())
+        .with_child(
+            Flex::row()
+                .with_child(
+                    Button::new("Settings")
+                        .on_click(|ctx, data: &mut AppState, _env| {
+                            ctx.submit_command(CHANGE_SETTINGS.with(!data.settings));
+                            ctx.request_update()
+                        })
+                        .align_left(),
+                )
+                .with_child(
+                    Button::new("Clear")
+                        .on_click(|ctx, _: &mut AppState, _env| {
+                            ctx.submit_command(CLEAR_DB);
+                            ctx.request_update()
+                        })
+                        .align_right(),
+                ),
+        )
+        .with_child(
+            Label::raw()
+                .with_font(FontDescriptor::new(FontFamily::MONOSPACE))
+                .lens(AppState::query_time)
+                .align_left(),
+        )
+        .with_child(
+            Label::raw()
+                .with_font(FontDescriptor::new(FontFamily::MONOSPACE))
+                .lens(AppState::count)
+                .align_left(),
+        )
+        .with_child(
+            Label::raw()
+                .with_font(FontDescriptor::new(FontFamily::MONOSPACE))
+                .lens(AppState::size)
+                .align_left(),
+        )
+        .with_child(
+            Label::raw()
+                .with_font(FontDescriptor::new(FontFamily::MONOSPACE))
+                .lens(AppState::indexed_data_in_bytes_string)
+                .align_left(),
+        )
+        .with_child(
+            Label::raw()
+                .with_font(FontDescriptor::new(FontFamily::MONOSPACE))
+                .lens(AppState::prob)
+                .align_left(),
+        )
+        .with_child(
+            Label::dynamic(|value: &AppState, _| format!("Index prob   {:.2}", value.index_prob))
+                .with_font(FontDescriptor::new(FontFamily::MONOSPACE))
+                .align_left(),
+        )
+        .with_child(
+            Slider::new()
+                .with_range(0.01, 0.8)
+                .with_step(0.01)
+                .lens(AppState::index_prob)
+                .align_left(),
+        )
+        .with_child(
+            Button::new("Set index prob")
+                .on_click(|_, data: &mut AppState, _env| {
+                    data.tx
+                        .send(CommandMessage::SetProb(data.index_prob.clone()))
+                        .unwrap();
+                })
+                .align_left(),
+        )
+        .with_child(
+            Label::dynamic(|value: &AppState, _| {
+                format!("Time limit   {:?} ms", value.timelimit as u64)
+            })
+            .with_font(FontDescriptor::new(FontFamily::MONOSPACE))
+            .align_left(),
+        )
+        .with_child(
+            Slider::new()
+                .with_range(10.0, 10000.0)
+                .with_step(1.0)
+                .lens(AppState::timelimit)
+                .align_left(),
+        )
+        .with_child(
+            Label::dynamic(|value: &AppState, _| {
+                format!("View limit   {:?}", value.viewlimit as u64)
+            })
+            .with_font(FontDescriptor::new(FontFamily::MONOSPACE))
+            .align_left(),
+        )
+        .with_child(
+            Slider::new()
+                .with_range(100.0, 50000.0)
+                .with_step(1.0)
+                .lens(AppState::viewlimit)
+                .align_left(),
+        )
         .with_child(new_search_textbox())
         .with_flex_child(Scroll::new(items).vertical(), 1.);
 
     let container = Container::new(
         Split::columns(
-            flex, Scroll::new(TextBox::multiline().with_font(FontDescriptor::new(FontFamily::MONOSPACE)).with_line_wrapping(true).lens(AppState::view).expand_width()).vertical(),
-        ).min_size(700., 1000.).split_point(0.2).draggable(true).solid_bar(true),
+            flex,
+            Scroll::new(
+                TextBox::multiline()
+                    .with_font(FontDescriptor::new(FontFamily::MONOSPACE))
+                    .with_line_wrapping(true)
+                    .lens(AppState::view)
+                    .expand_width(),
+            )
+            .vertical(),
+        )
+        .min_size(700., 1000.)
+        .split_point(0.2)
+        .draggable(true)
+        .solid_bar(true),
     );
 
     let flex_settings = Flex::column()
         .with_child(
-            Button::new("Close settings").on_click(|ctx, data: &mut AppState, _env| {
-                ctx.submit_command(CHANGE_SETTINGS.with(!data.settings));
-                ctx.request_update();
-            }).align_left())
+            Button::new("Close settings")
+                .on_click(|ctx, data: &mut AppState, _env| {
+                    ctx.submit_command(CHANGE_SETTINGS.with(!data.settings));
+                    ctx.request_update();
+                })
+                .align_left(),
+        )
         .with_child(
-            Button::new("Clear settings").on_click(|ctx, data: &mut AppState, _env| {
-                data.pointers.clear();
-                data.view_column = "".to_string();
-                ctx.request_update();
-            }).align_left())
-        .with_flex_child(Scroll::new(List::new(|| {
-            Flex::row()
-                .with_child(Checkbox::new("").lens(PointerState::checked)
-                    .on_click(|ctx, pointer_state, _env| {
-                        pointer_state.checked = !pointer_state.checked;
-                        let state = pointer_state.clone();
-                        ctx.submit_command(CHECK_CLICKED_FOR_POINTER.with(state));
-                    })
-                )
-                .with_child(Label::new(|item: &PointerState, _env: &_| format!("{}", item.text))
-                )
-        }))
-                             .vertical()
-                             .lens(AppState::pointers).align_left(), 1.)
+            Button::new("Clear settings")
+                .on_click(|ctx, data: &mut AppState, _env| {
+                    data.pointers.clear();
+                    data.view_column = "".to_string();
+                    ctx.request_update();
+                })
+                .align_left(),
+        )
+        .with_flex_child(
+            Scroll::new(List::new(|| {
+                Flex::row()
+                    .with_child(Checkbox::new("").lens(PointerState::checked).on_click(
+                        |ctx, pointer_state, _env| {
+                            pointer_state.checked = !pointer_state.checked;
+                            let state = pointer_state.clone();
+                            ctx.submit_command(CHECK_CLICKED_FOR_POINTER.with(state));
+                        },
+                    ))
+                    .with_child(Label::new(|item: &PointerState, _env: &_| {
+                        format!("{}", item.text)
+                    }))
+            }))
+            .vertical()
+            .lens(AppState::pointers)
+            .align_left(),
+            1.,
+        )
         .with_child(Label::new("Select view tag:").padding(8.0).align_left())
-        .with_flex_child(Scroll::new(List::new(|| {
-            Label::new(|item: &PointerState, _env: &_| format!("{}", item.text)).on_click(|ctx, item, _env| {
-                ctx.submit_command(SET_VIEW_COLUMN.with(item.text.to_string()));
-            })
-        }))
-                             .vertical()
-                             .lens(AppState::pointers).align_left(), 1.);
+        .with_flex_child(
+            Scroll::new(List::new(|| {
+                Label::new(|item: &PointerState, _env: &_| format!("{}", item.text)).on_click(
+                    |ctx, item, _env| {
+                        ctx.submit_command(SET_VIEW_COLUMN.with(item.text.to_string()));
+                    },
+                )
+            }))
+            .vertical()
+            .lens(AppState::pointers)
+            .align_left(),
+            1.,
+        );
 
     let either = Either::new(
         |data: &AppState, _env| data.settings,
